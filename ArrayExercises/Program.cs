f1f8feffd4d69c241b7f0d8dacb9055b59046f2b
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-10 18:51:46 -0500

    it's mine now!

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,7 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            Console.WriteLine("Hello  Arrays!");
         }
     }
 }
