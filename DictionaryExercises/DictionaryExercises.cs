77079d9b7a0f3640901928745760c4d7547ae0ef
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-11 01:44:04 -0500

    dictionary exercises

---
+++ DictionaryExercises/DictionaryExercises.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DictionaryExercises
+{
+    class DictionaryExercises
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+
+            // Get student names and grades
+            do
+            {
+                Console.WriteLine("Student: ");
+                string input = Console.ReadLine();
+                newStudent = input;
+
+                if (!Equals(newStudent, ""))
+                {
+                    Console.WriteLine("ID: ");
+                    input = Console.ReadLine();
+                    double id = double.Parse(input);
+                    students.Add(newStudent, id);
+
+                    // Read in the newline before looping back
+                    Console.ReadLine();
+                }
+
+            } while (!Equals(newStudent, ""));
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<string, double> student in students)
+            {
+                Console.WriteLine(student.Key + " (" + student.Value + ")");
+                sum += student.Value;
+            }
+        }
+    }
+}
+
+
+/*
+ * Make a program similar to GradebookDictionary that does the following:
+
+It takes in student names and ID numbers (as integers) instead of names and grades.
+The keys should be the IDs and the values should be the names.
+Modify the roster printing code accordingly.
+ * */
